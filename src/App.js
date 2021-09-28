0230a58fd0d053e0586a66f83172359d2ea86a4f
Carlos Gimenez <gimenez.22axl@gmail.com>
2021-09-28 19:12:55 -0300

    funciona

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from "react";
-import { Map } from "maplibre-gl";
+import { Map, Marker } from "maplibre-gl";
 import "./App.css";
 //Prueba como si no funcionara
 function App() {
   useEffect(() => {
-    new Map({
+    let map = new Map({
       container: "mapContainer", // container id
       style: "https://demotiles.maplibre.org/style.json", // style URL
-      center: [-80, -38 ], // starting position [lng, lat]
+      center: [-80, -38], // starting position [lng, lat]
       zoom: 3, // starting zoom
     });
+    let marker = new Marker().setLngLat([-80, -37]).addTo(map);
   }, []);
 
   return (

41c5b09b8b39a55e1f131b139d54f0dcde4eda61
Carlos Gimenez <gimenez.22axl@gmail.com>
2021-09-28 17:48:09 -0300

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React, { useEffect } from "react";
 import { Map } from "maplibre-gl";
-import './App.css';
+import "./App.css";
 
 function App() {
   useEffect(() => {
-     new Map({
+    new Map({
       container: "mapContainer", // container id
       style: "https://demotiles.maplibre.org/style.json", // style URL
-      center: [-58, -34], // starting position [lng, lat]
+      center: [-80, -38 ], // starting position [lng, lat]
       zoom: 3, // starting zoom
     });
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-      <div>
-      <div style={{height:'100vh'}} id="mapContainer"></div>
-    </div>
-      </header>
+    <div>
+      <div style={{ height: "100vh" }} id="mapContainer"></div>
     </div>
   );
 }
---
+++ src/Marker/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import { Map } from "maplibre-gl";
+
+const Market = () => {
+    const [market, setMarket] = useState(false)
+    
+    const Punto = () => {
+        setMarket(!market)
+    }
+    useEffect((market) => {
+        new Map ({
+          setMarket,
+          container: "mapContainer", // container id
+          style: "https://demotiles.maplibre.org/style.json", // style URL
+          center: [-80, -38 ], // starting position [lng, lat]
+          zoom: 3, // starting zoom
+        });
+      }, []);
+
+    return (
+        <div>
+           <Punto onclik={Punto} Marcador />
+        </div>
+    )
+}
+
+export default Market

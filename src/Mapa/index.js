0f5cb73b417775d73ab84ed5dd54f23d49ccb2fe
Carlos Gimenez <gimenez.22axl@gmail.com>
2021-10-01 23:49:33 -0300

    se cambio el style por style raster

--- src/Mapa/index.js
+++ src/Mapa/index.js
@@ -1,13 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { Map } from "maplibre-gl";
 import MarkerComponent from "../Marker";
+// import Raster from "../Raster";
 
 const MapUnico = () => {
   const [map, setMap] = useState(null);
   useEffect(() => {
     setMap(new Map({
       container: "mapContainer", // container id
-      style: "https://demotiles.maplibre.org/style.json", // style URL
+      // con el style se puede cambiar la capa del mapa
+      style: {
+        version: 8,
+        sources: {
+          "raster-tiles": {
+            type: "raster",
+            tiles: [
+              "https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg",
+            ],
+            tileSize: 256,
+            attribution:
+              'Map tiles by <a target="_top" rel="noopener" href="http://stamen.com">Stamen Design</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>',
+          },
+        },
+        layers: [
+          {
+            id: "simple-tiles",
+            type: "raster",
+            source: "raster-tiles",
+            minzoom: 0,
+            maxzoom: 22,
+          },
+        ],
+      }, // style URL
       center: [-80, -38], // starting position [lng, lat]
       zoom: 3, // starting zoom
     }))

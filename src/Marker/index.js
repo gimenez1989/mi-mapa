807aa7615af17df970bbfb3c39664bd031ac3f7c
Carlos Gimenez <gimenez.22axl@gmail.com>
2021-10-08 16:21:33 -0300

    se agregor marcador arrastable

--- src/Marker/index.js
+++ src/Marker/index.js
@@ -1,16 +1,37 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Marker } from "maplibre-gl";
 
 const MarkerComponent = ({ map }) => {
+  const [marker, setMarker] = useState(null);
+  const coordinates = document.getElementById("coordinates");
+
   useEffect(() => {
-    new Marker({
-      draggable: true
-    })
-    .setLngLat([-58.45, -34.62])
-    .addTo(map);
+    setMarker(
+      new Marker({
+        draggable: true,
+      })
+        .setLngLat([-58.45, -34.59])
+        .addTo(map)
+    );
+    //new Marker().setLngLat([-58.45, -34.59]).addTo(map);
   }, [map]);
 
-  return <div></div>;
+  useEffect(() => {
+    
+    const onDragEnd = () => {
+      const lngLat = marker.getLngLat();
+      coordinates.style.display = "block";
+      coordinates.innerHTML =
+        "Longitude: " + lngLat.lng + "<br />Latitude: " + lngLat.lat;
+    };
+    
+    if(marker != null ) {
+      marker.on("dragend", onDragEnd);
+    }
+
+  }, [marker])
+
+  return null;
 };
 
 export default MarkerComponent;

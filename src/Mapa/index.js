9a9d329c5e1b2adb14d6183ea9154b49af0aeb59
Carlos Gimenez <gimenez.22axl@gmail.com>
2021-10-09 14:04:00 -0300

    se le agrego una clase y estilos al boton de capas

--- src/Mapa/index.js
+++ src/Mapa/index.js
@@ -81,7 +81,7 @@ const MapUnico = () => {
       {map && <Route map={map} />}
 
       {map && (
-        <button
+        <button className="buttonCapas"
           onClick={() => {
             change();
           }}
